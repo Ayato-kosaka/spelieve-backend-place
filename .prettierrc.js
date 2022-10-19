0dd8218ae9221ad4e835c516fbaa066acb93b2e1
Ayato-kosaka <53715839+Ayato-kosaka@users.noreply.github.com>
2022-10-19 16:15:48 +0900

    Update and rename .prettierrc to .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120, // Specify the line length that the printer will wrap on.
+  tabWidth: 2, // Specify the number of spaces per indentation-level.
+  useTabs: true, // Indent lines with tabs instead of spaces.
+  semi: true, // Print semicolons at the ends of statements.
+  singleQuote: true, // Use single quotes instead of double quotes.
+  quoteProps: "as-needed", // Change when properties in objects are quoted.
+  jsxSingleQuote: false, // Use single quotes instead of double quotes in JSX.
+  trailingComma: "all", // Print trailing commas wherever possible in multi-line comma-separated syntactic structures. 
+  bracketSpacing: true, // Print spaces between brackets in object literals.
+  bracketSameLine: true, // Put the > of a multi-line HTML (HTML, JSX, Vue, Angular) element at the end of the last line instead of being alone on the next line (does not apply to self closing elements).
+  arrowParens: "always", // Include parentheses around a sole arrow function parameter.
+}
