51dbeec4e9caaa3e41d039770d56732978bdf17f
Takapy1 <75763679+Takapy1@users.noreply.github.com>
2022-10-23 14:09:23 +0900

    Update PBL002UpsertPlaceData.service.ts
    @Ayato-kosaka 
    ごめん、レビューしたの俺なのに追加で修正すべきものがあったから、proposeするわ
    - Detailsのtypesのadministrative関連はスネークケースで来る
    - sublocalityはlevelはなく一つしかない

--- src/PBL002UpsertPlaceData/PBL002UpsertPlaceData.service.ts
+++ src/PBL002UpsertPlaceData/PBL002UpsertPlaceData.service.ts
@@ -68,26 +68,27 @@ export class PBL002UpsertPlaceDataService {
 				address_component.types.includes('country'),
 			)?.long_name as string,
 			administrativeAreaLevel1: googlePlaceDetailsResult.address_components.find((address_component) =>
-				address_component.types.includes('administrativeAreaLevel1'),
+				address_component.types.includes('administrative_area_level_1'),
 			)?.long_name,
 			administrativeAreaLevel2: googlePlaceDetailsResult.address_components.find((address_component) =>
-				address_component.types.includes('administrativeAreaLevel2'),
+				address_component.types.includes('administrative_area_level_2'),
 			)?.long_name,
 			locality: googlePlaceDetailsResult.address_components.find((address_component) =>
 				address_component.types.includes('locality'),
 			)?.long_name,
-			sublocalityLevel1: googlePlaceDetailsResult.address_components.find((address_component) =>
-				address_component.types.includes('sublocalityLevel1'),
+			// REVIEW: sublocalityは一つだけ（sublocality_levelはないと思う）
+			sublocality: googlePlaceDetailsResult.address_components.find((address_component) =>
+				address_component.types.includes('sublocality'),
 			)?.long_name,
-			sublocalityLevel2: googlePlaceDetailsResult.address_components.find((address_component) =>
-				address_component.types.includes('sublocalityLevel2'),
-			)?.long_name,
-			sublocalityLevel3: googlePlaceDetailsResult.address_components.find((address_component) =>
-				address_component.types.includes('sublocalityLevel3'),
-			)?.long_name,
-			sublocalityLevel4: googlePlaceDetailsResult.address_components.find((address_component) =>
-				address_component.types.includes('sublocalityLevel4'),
-			)?.long_name,
+			//sublocalityLevel2: googlePlaceDetailsResult.address_components.find((address_component) =>
+			//	address_component.types.includes('sublocalityLevel2'),
+			//)?.long_name,
+			//sublocalityLevel3: googlePlaceDetailsResult.address_components.find((address_component) =>
+			//	address_component.types.includes('sublocalityLevel3'),
+			//)?.long_name,
+			//sublocalityLevel4: googlePlaceDetailsResult.address_components.find((address_component) =>
+			//	address_component.types.includes('sublocalityLevel4'),
+			//)?.long_name,
 			premise: googlePlaceDetailsResult.address_components.find((address_component) =>
 				address_component.types.includes('premise'),
 			)?.long_name,
