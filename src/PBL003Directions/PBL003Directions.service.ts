f0f7f1f566afd7d18fb3097762794db3f231aec7
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-12-03 02:12:28 +0000

    Update PBL003DirectionsService

--- src/PBL003Directions/PBL003Directions.service.ts
+++ src/PBL003Directions/PBL003Directions.service.ts
@@ -6,10 +6,13 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class PBL003DirectionsService {
 	constructor(private configService: ConfigService) {}
-	async doExecute(body: Omit<DirectionsRequest['params'], 'key'>) {
+	doExecute(body: Omit<DirectionsRequest['params'], 'key'>) {
 		const client = new Client({});
-		return await client.directions({
-			params: { ...body, key: this.configService.get<string>('GOOGLE_CLOUD_API_KEY')! },
-		});
+		return client
+			.directions({
+				params: { ...body, key: this.configService.get<string>('GOOGLE_CLOUD_API_KEY')! },
+			})
+			.then((directionsResponse) => directionsResponse)
+			.catch((e) => e);
 	}
 }
