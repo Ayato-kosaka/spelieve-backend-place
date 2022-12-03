a8e6f503d9554b85f95fe78d4fbeedba899f2044
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-12-03 05:42:30 +0000

    Update PBL003DirectionsService

--- src/PBL003Directions/PBL003Directions.service.ts
+++ src/PBL003Directions/PBL003Directions.service.ts
@@ -1,4 +1,4 @@
-import { Client, DirectionsRequest } from '@googlemaps/google-maps-services-js';
+import { Client, DirectionsRequest, DirectionsResponse } from '@googlemaps/google-maps-services-js';
 
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
@@ -6,13 +6,16 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class PBL003DirectionsService {
 	constructor(private configService: ConfigService) {}
-	doExecute(body: Omit<DirectionsRequest['params'], 'key'>) {
+	async doExecute(
+		body: Omit<DirectionsRequest['params'], 'key'>,
+	): Promise<Pick<DirectionsResponse, 'status' | 'data'>> {
 		const client = new Client({});
-		return client
-			.directions({
-				params: { ...body, key: this.configService.get<string>('GOOGLE_CLOUD_API_KEY')! },
-			})
-			.then((directionsResponse) => directionsResponse)
-			.catch((e) => e);
+		const { status, data } = await client.directions({
+			params: { ...body, key: this.configService.get<string>('GOOGLE_CLOUD_API_KEY')! },
+		});
+		return {
+			status,
+			data,
+		};
 	}
 }
