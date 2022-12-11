7e4a0ec2a53f0e6a59269df50108e1748e2ac83d
Takapy1 <takahirog1109mai@gmail.com>
2022-12-11 21:14:38 +0900

    [success] fetch place data details

---
+++ src/PBL004PlaceAutocomplete/PBL004PlaceAutocomplete.service.ts
@@ -0,0 +1,29 @@
+import { Client, PlaceAutocompleteRequest, PlaceAutocompleteResponse } from '@googlemaps/google-maps-services-js';
+
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PBL004PlaceAutocompleteService {
+	constructor(private configService: ConfigService) {}
+	async doExecute(
+		body: Omit<PlaceAutocompleteRequest['params'], 'key'>,
+	): Promise<Pick<PlaceAutocompleteResponse, 'status' | 'data'>> {
+		const client = new Client({});
+		const { status, data } = await client
+			.placeAutocomplete({
+				params: { ...body, key: this.configService.get<string>('GOOGLE_CLOUD_API_KEY')! },
+			})
+			.then((value) => {
+				return value;
+			})
+			.catch((e) => {
+				console.log(e);
+				return e.response;
+			});
+		return {
+			status,
+			data,
+		};
+	}
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { PBL002UpsertPlaceDataModule } from './PBL002UpsertPlaceData/PBL002UpsertPlaceData.module';
 import { PBL003DirectionsModule } from './PBL003Directions/PBL003Directions.module';
+import { PBL004PlaceAutocompleteModule } from './PBL004PlaceAutocomplete/PBL004PlaceAutocomplete.module';
 
 @Module({
 	imports: [
@@ -12,6 +13,7 @@ import { PBL003DirectionsModule } from './PBL003Directions/PBL003Directions.modu
 		}),
 		PBL002UpsertPlaceDataModule,
 		PBL003DirectionsModule,
+		PBL004PlaceAutocompleteModule,
 	],
 })
 export class AppModule {}
