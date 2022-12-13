a8f1af9465f2f0eadd7ec5043442c76769b7f65d
Ayato-kosaka <kosaka.ayato@gmail.com>
2022-12-13 15:35:11 +0000

    ポート番号変更に対応

--- src/PBL002UpsertPlaceData/PBL002UpsertPlaceData.controller.ts
+++ src/PBL002UpsertPlaceData/PBL002UpsertPlaceData.controller.ts
@@ -8,7 +8,7 @@ import { PBL002UpsertPlaceDataService } from './PBL002UpsertPlaceData.service';
 export class PBL002UpsertPlaceDataController {
 	constructor(private readonly service: PBL002UpsertPlaceDataService) {}
 
-	// curl -XPOST -H "Content-Type:application/json" localhost:3000/PBL002 -d '{"place_id":"ChIJD4CMmg1cGGARaEcYbfRRgSk", "language":"ja"}'
+	// curl -XPOST -H "Content-Type:application/json" localhost:3080/PBL002 -d '{"place_id":"ChIJD4CMmg1cGGARaEcYbfRRgSk", "language":"ja"}'
 	@Post()
 	PBL002(@Body() PBL002Body: UpsertPlaceDataBodyInterface) {
 		return this.service.doExecute(PBL002Body);
